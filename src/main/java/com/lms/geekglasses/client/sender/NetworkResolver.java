0b22a723dff65aded08603b73ff6f51707b93677
Yaroslav <56473255+Serwios@users.noreply.github.com>
2024-03-29 13:29:30 +0200

    Update NetworkResolver.java

--- src/main/java/com/lms/geekglasses/client/sender/NetworkResolver.java
+++ src/main/java/com/lms/geekglasses/client/sender/NetworkResolver.java
@@ -9,33 +9,38 @@ import java.util.List;
 public class NetworkResolver {
     private static final String SUBNET = "192.168.0";
 
-    public List<String> resolveAvailableComputersIps() throws IOException {
+    public List<String> resolveAvailableComputersIps() {
         List<String> listOfIps = new ArrayList<>();
-        System.out.println("Please wait, I try to resolve nearby computers");
+        System.out.println("Please wait, attempting to resolve nearby computers...");
 
         final int timeout = 100;
         for (int i = 1; i < 255; i++) {
             String host = SUBNET + "." + i;
-            InetAddress inetAddress = InetAddress.getByName(host);
-
-            if (inetAddress.isReachable(timeout)) {
-                listOfIps.add(inetAddress.toString());
+            try {
+                InetAddress inetAddress = InetAddress.getByName(host);
+                if (inetAddress.isReachable(timeout)) {
+                    listOfIps.add(inetAddress.getHostAddress());
+                }
+            } catch (IOException e) {
+                // Handle or log any IOException that may occur during InetAddress resolution
+                System.err.println("Error resolving IP address: " + e.getMessage());
             }
         }
 
         if (listOfIps.isEmpty()) {
-            System.out.println("There is no available computers in network");
+            System.out.println("There are no available computers in the network.");
         }
 
         return listOfIps;
     }
 
     public String getCurrentIpAddress() {
-        try(final DatagramSocket socket = new DatagramSocket()){
+        try (final DatagramSocket socket = new DatagramSocket()) {
             socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
             return socket.getLocalAddress().getHostAddress();
-        } catch (Throwable e) {
-            System.out.println("Failed to get local machine address" + e);
+        } catch (IOException e) {
+            // Handle or log any IOException that may occur during socket creation
+            System.err.println("Failed to get local machine address: " + e.getMessage());
             return "";
         }
     }
